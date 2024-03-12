395ae3b294ed2aa549db252df46970430f307fa3
caiofeuser <caiofeuserdm@gmail.com>
2024-03-12 13:01:51 -0300

    starting project

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 ﻿// fileName : server.js
 // Example using the http module
-const http = require("http");
+import { createServer } from "http";
+import { Express } from "express";
 
 // Create an HTTP server
-const server = http.createServer((req, res) => {
-  // Set the response headers
-  res.writeHead(200, { "Content-Type": "text/html" });
-
-  // Write the response content
-  res.write("<h1>Hello, Node.js HTTP Server!</h1>");
-  res.end();
+const app = new Express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-// Specify the port to listen on
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Start the server
-server.listen(port, () => {
-  console.log(`Node.js HTTP server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
